e93d19f0259782c8387049362bea310cb217f868
NWorthington <nworthington@amarillocollege.com>
2019-12-10 15:24:07 -0600

    Added User forms provided by Rosie.

--- CareAmarillo/Form1.Designer.cs
+++ CareAmarillo/Form1.Designer.cs
@@ -28,20 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtSearchBar = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // lblName
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(237, 41);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(324, 59);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "CareAmarillo";
+            // 
+            // txtSearchBar
+            // 
+            this.txtSearchBar.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtSearchBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchBar.Location = new System.Drawing.Point(163, 152);
+            this.txtSearchBar.Name = "txtSearchBar";
+            this.txtSearchBar.Size = new System.Drawing.Size(371, 30);
+            this.txtSearchBar.TabIndex = 1;
+            this.txtSearchBar.Text = "ENTER A SEARCH TERM";
+            this.txtSearchBar.UseWaitCursor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(575, 144);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 43);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // CareAmarillo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchBar);
+            this.Controls.Add(this.lblName);
+            this.Name = "CareAmarillo";
+            this.Text = "CareAmarillo";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtSearchBar;
+        private System.Windows.Forms.Button btnSearch;
     }
 }
 
--- CareAmarillo/Form1.cs
+++ CareAmarillo/Form1.cs
@@ -17,5 +17,43 @@ namespace CareAmarillo
             InitializeComponent();
             // Testing test connection DJH
         }
+        Location locationSearchResults = new Location();
+        public static string GetValue = "";
+
+
+        private void BtnSearch_Click(object sender, EventArgs e)    //when the user presses the 'search' button, 
+                                                                    //the search screen will be presented
+        {
+            if (txtSearchBar.Text == ("LOCATION"))
+            {
+                locationSearchResults.Show();
+                this.Hide();
+            }
+            if (txtSearchBar.Text == ("LOCATIONS"))
+            {
+                locationSearchResults.Show();
+                this.Hide();
+            }
+            if (txtSearchBar.Text == ("ADRESS"))
+            {
+                locationSearchResults.Show();
+                this.Hide();
+            }
+            if (txtSearchBar.Text == ("ADRESSES"))
+            {
+                locationSearchResults.Show();
+                this.Hide();
+            }
+            if (txtSearchBar.Text == ("SHELTERS"))
+            {
+                locationSearchResults.Show();
+                this.Hide();
+            }
+            if (txtSearchBar.Text == ("EMERGENCY SHELTERS"))
+            {
+                locationSearchResults.Show();
+                this.Hide();
+            }
+        }
     }
 }
