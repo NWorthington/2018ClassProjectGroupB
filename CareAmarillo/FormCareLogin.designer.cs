aae896c9df0c5936597fa401e1b64fa38fd5260a
Oscar Martinez <omartinez36@amarillocollege.com>
2019-12-13 18:36:39 -0600

    Login GUI fully comepleted

--- CareAmarillo/FormCareLogin.designer.cs
+++ CareAmarillo/FormCareLogin.designer.cs
@@ -36,13 +36,24 @@
             this.plUnderline = new System.Windows.Forms.Panel();
             this.tbxUsername = new System.Windows.Forms.TextBox();
             this.tbxPassword = new System.Windows.Forms.TextBox();
-            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblNewPassword = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.lblForgot = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblConPassword = new System.Windows.Forms.Label();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.pbxRed2 = new System.Windows.Forms.PictureBox();
+            this.pbxRed1 = new System.Windows.Forms.PictureBox();
+            this.pbxCheck2 = new System.Windows.Forms.PictureBox();
+            this.pbxCheck1 = new System.Windows.Forms.PictureBox();
             this.plTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxCareText)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxCareLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRed2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRed1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCheck2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCheck1)).BeginInit();
             this.SuspendLayout();
             // 
             // plTop
@@ -118,16 +129,16 @@
             this.tbxPassword.Size = new System.Drawing.Size(573, 58);
             this.tbxPassword.TabIndex = 4;
             // 
-            // lblUsername
+            // lblNewPassword
             // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lblUsername.Location = new System.Drawing.Point(72, 317);
-            this.lblUsername.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(145, 32);
-            this.lblUsername.TabIndex = 5;
-            this.lblUsername.Text = "Username";
+            this.lblNewPassword.AutoSize = true;
+            this.lblNewPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblNewPassword.Location = new System.Drawing.Point(72, 323);
+            this.lblNewPassword.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblNewPassword.Name = "lblNewPassword";
+            this.lblNewPassword.Size = new System.Drawing.Size(202, 32);
+            this.lblNewPassword.TabIndex = 5;
+            this.lblNewPassword.Text = "New Password";
             // 
             // lblPassword
             // 
@@ -166,21 +177,107 @@
             this.lblForgot.TabIndex = 9;
             this.lblForgot.Text = "Forgot Password?";
             // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblUsername.Location = new System.Drawing.Point(74, 323);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(145, 32);
+            this.lblUsername.TabIndex = 10;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblConPassword
+            // 
+            this.lblConPassword.AutoSize = true;
+            this.lblConPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblConPassword.Location = new System.Drawing.Point(74, 505);
+            this.lblConPassword.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblConPassword.Name = "lblConPassword";
+            this.lblConPassword.Size = new System.Drawing.Size(245, 32);
+            this.lblConPassword.TabIndex = 11;
+            this.lblConPassword.Text = "Confirm Password";
+            // 
+            // btnDone
+            // 
+            this.btnDone.BackColor = System.Drawing.Color.White;
+            this.btnDone.FlatAppearance.BorderSize = 0;
+            this.btnDone.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.btnDone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDone.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnDone.Location = new System.Drawing.Point(80, 715);
+            this.btnDone.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(245, 79);
+            this.btnDone.TabIndex = 12;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = false;
+            // 
+            // pbxRed2
+            // 
+            this.pbxRed2.Image = ((System.Drawing.Image)(resources.GetObject("pbxRed2.Image")));
+            this.pbxRed2.Location = new System.Drawing.Point(540, 505);
+            this.pbxRed2.Name = "pbxRed2";
+            this.pbxRed2.Size = new System.Drawing.Size(41, 33);
+            this.pbxRed2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxRed2.TabIndex = 13;
+            this.pbxRed2.TabStop = false;
+            // 
+            // pbxRed1
+            // 
+            this.pbxRed1.Image = ((System.Drawing.Image)(resources.GetObject("pbxRed1.Image")));
+            this.pbxRed1.Location = new System.Drawing.Point(540, 319);
+            this.pbxRed1.Name = "pbxRed1";
+            this.pbxRed1.Size = new System.Drawing.Size(41, 33);
+            this.pbxRed1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxRed1.TabIndex = 14;
+            this.pbxRed1.TabStop = false;
+            // 
+            // pbxCheck2
+            // 
+            this.pbxCheck2.BackColor = System.Drawing.Color.Transparent;
+            this.pbxCheck2.Image = ((System.Drawing.Image)(resources.GetObject("pbxCheck2.Image")));
+            this.pbxCheck2.Location = new System.Drawing.Point(540, 504);
+            this.pbxCheck2.Name = "pbxCheck2";
+            this.pbxCheck2.Size = new System.Drawing.Size(41, 33);
+            this.pbxCheck2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxCheck2.TabIndex = 15;
+            this.pbxCheck2.TabStop = false;
+            // 
+            // pbxCheck1
+            // 
+            this.pbxCheck1.BackColor = System.Drawing.Color.Transparent;
+            this.pbxCheck1.Image = ((System.Drawing.Image)(resources.GetObject("pbxCheck1.Image")));
+            this.pbxCheck1.Location = new System.Drawing.Point(540, 319);
+            this.pbxCheck1.Name = "pbxCheck1";
+            this.pbxCheck1.Size = new System.Drawing.Size(41, 33);
+            this.pbxCheck1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxCheck1.TabIndex = 16;
+            this.pbxCheck1.TabStop = false;
+            // 
             // FormCareLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(642, 856);
+            this.Controls.Add(this.pbxRed2);
+            this.Controls.Add(this.lblUsername);
             this.Controls.Add(this.lblForgot);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblNewPassword);
             this.Controls.Add(this.tbxPassword);
             this.Controls.Add(this.tbxUsername);
             this.Controls.Add(this.plUnderline);
             this.Controls.Add(this.plUnderline2);
             this.Controls.Add(this.plTop);
+            this.Controls.Add(this.lblConPassword);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.pbxCheck2);
+            this.Controls.Add(this.pbxCheck1);
+            this.Controls.Add(this.pbxRed1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "FormCareLogin";
@@ -189,6 +286,10 @@
             this.plTop.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbxCareText)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxCareLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRed2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRed1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCheck2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCheck1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,11 +302,18 @@
         private System.Windows.Forms.Panel plUnderline;
         private System.Windows.Forms.TextBox tbxUsername;
         private System.Windows.Forms.TextBox tbxPassword;
-        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblNewPassword;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.PictureBox pbxCareText;
         private System.Windows.Forms.PictureBox pbxCareLogo;
         private System.Windows.Forms.Label lblForgot;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblConPassword;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.PictureBox pbxRed2;
+        private System.Windows.Forms.PictureBox pbxRed1;
+        private System.Windows.Forms.PictureBox pbxCheck2;
+        private System.Windows.Forms.PictureBox pbxCheck1;
     }
 }
