1f1f65e78cb987810b46feca8b9cf0b7dadd5cf7
Jonathan Lucatero <Lucatero_Jonathan@yahoo.com>
2019-12-16 17:43:46 -0600

    User Class SearchDatabase() code complete

--- CareAmarillo/User.cs
+++ CareAmarillo/User.cs
@@ -16,18 +16,45 @@ namespace CareAmarillo
         {
             if ( wordSearched != null)
             {
-                connection.ConnectionString = "Server=cis1.actx.edu;Database=project2;User Id=db2;Password=db20;";
-                connection.Open();
+                //connection.ConnectionString = "Server=cis1.actx.edu;Database=project2;User Id=db2;Password=db20;";
+                //connection.Open();
 
-                using (SqlCommand search = connection.CreateCommand())
+                //using (SqlCommand search = connection.CreateCommand())
+                //{
+                //    System.Diagnostics.Debug.WriteLine(connection.ServerVersion);
+
+                //    search.CommandText = "select ..."; // haven't figure the SQL code out.
+
+
+                //    connection.Close();
+                //}
+                var testForEmergencyServices = DatabaseProcess.DataSetHumanServices();
+                var subset1 = testForEmergencyServices.Tables[0].Select("CompanyName >= " + wordSearched + 
+                                                                        " or HEmail >= " + wordSearched + 
+                                                                        " or HPhone >= " + wordSearched + 
+                                                                        " or HCity" + wordSearched + 
+                                                                        " or HState" + wordSearched +
+                                                                        " or HAddress" + wordSearched);
+                // Print column 0 of each returned row.
+                for (int i = 0; i < subset1.Length; i++)
                 {
-                    System.Diagnostics.Debug.WriteLine(connection.ServerVersion);
-
-                    search.CommandText = "select ..."; // haven't figure the SQL code out.
-
-
-                    connection.Close();
+                    Console.WriteLine(subset1[i]);
                 }
+
+                var testForHumanServices = DatabaseProcess.DataSetHumanServices();
+                var subset2 = testForHumanServices.Tables[0].Select("CompanyName >= " + wordSearched +
+                                                                        " or HEmail >= " + wordSearched +
+                                                                        " or HPhone >= " + wordSearched +
+                                                                        " or HCity" + wordSearched +
+                                                                        " or HState" + wordSearched +
+                                                                        " or HAddress" + wordSearched);
+                // Print column 0 of each returned row.
+                for (int i = 0; i < subset2.Length; i++)
+                {
+                    Console.WriteLine(subset2[i]);
+                }
+
+
             }
         }
     }
