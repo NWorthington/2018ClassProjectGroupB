0889b6d1a6f12e0b1e81a8ae175dae6b280685a0
Oscar Martinez <omartinez36@amarillocollege.com>
2019-12-17 16:47:32 -0600

    Changed run to start up form

--- CareAmarillo/Program.cs
+++ CareAmarillo/Program.cs
@@ -37,7 +37,7 @@ namespace CareAmarillo
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormUniSearch());
+            Application.Run(new formCareAmarillo());
 
             // The methods for SQL interaction.
 
